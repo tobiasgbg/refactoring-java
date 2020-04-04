8f6d92ce01574c2656a039a40191ae9fb44010e8
Tobias Adriansson <tobias.gbg@gmail.com>
2020-04-04 22:36:44 +0200

    create movie factory

--- src/MovieLibrary.java
+++ src/MovieLibrary.java
@@ -1,17 +1,14 @@
 import java.util.HashMap;
 
 public abstract class MovieLibrary {
-    private HashMap<String, Movie> movies = new HashMap<String, Movie>();
+    public HashMap<String, Movie> movies = new HashMap<String, Movie>();
 
     public MovieLibrary() {
-
-        movies.put("F001", new RegularMovie("You've Got Mail"));
-        movies.put("F002", new RegularMovie("Matrix"));
-        movies.put("F003", new ChildrensMovie("Cars"));
-        movies.put("F004", new NewMovie("Fast & Furious X"));
     }
 
     public HashMap<String, Movie> getMovies() {
         return movies;
     }
+
+    protected abstract Movie createMovie(String name, String type);
 }
--- src/RentalMovieLibrary.java
+++ src/RentalMovieLibrary.java
@@ -2,8 +2,11 @@ public class RentalMovieLibrary extends MovieLibrary {
     private static RentalMovieLibrary instance;
 
     private RentalMovieLibrary() {
-
-        super();
+        
+        movies.put("F001", createMovie("You've Got Mail", "regular"));
+        movies.put("F002", createMovie("Matrix", "regular"));
+        movies.put("F003", createMovie("Cars", "childrens"));
+        movies.put("F004", createMovie("Fast & Furious X", "new"));
     }
 
     public static RentalMovieLibrary getInstance()
@@ -13,5 +16,18 @@ public class RentalMovieLibrary extends MovieLibrary {
             instance = new RentalMovieLibrary();
         }
         return instance;
+    }
+
+    protected Movie createMovie(String title, String type)
+    {
+        Movie movie = null;
+        if (type == "regular")
+            movie = new RegularMovie(title);
+        else if (type == "childrens")
+            movie = new ChildrensMovie(title);
+        else if (type == "new")
+            movie = new NewMovie(title);
+
+        return movie;
     }
 }
