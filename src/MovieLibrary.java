c74a0b32552759899152c3e5b6b622b5488f7194
Tobias Adriansson <tobias.gbg@gmail.com>
2020-04-04 11:53:00 +0200

    Turn Movielibrary into singleton

--- src/MovieLibrary.java
+++ src/MovieLibrary.java
@@ -2,13 +2,23 @@ import java.util.HashMap;
 
 public class MovieLibrary {
     private HashMap<String, Movie> movies = new HashMap<String, Movie>();
+    private static MovieLibrary instance;
 
-    public MovieLibrary() {
+    private MovieLibrary() {
 
         movies.put("F001", new Movie("You've Got Mail", "regular"));
         movies.put("F002", new Movie("Matrix", "regular"));
         movies.put("F003", new Movie("Cars", "childrens"));
         movies.put("F004", new Movie("Fast & Furious X", "new"));
+    }
+
+    public static MovieLibrary getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new MovieLibrary();
+        }
+        return instance;
     }
 
     public HashMap<String, Movie> getMovies() {
