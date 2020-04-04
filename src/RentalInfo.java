96da72628e01b75d1c9e7181b9dc47f8abc14601
Tobias Adriansson <tobias.gbg@gmail.com>
2020-04-04 11:38:30 +0200

    Add movie library based on the OOD principle of Abstraction

--- src/RentalInfo.java
+++ src/RentalInfo.java
@@ -3,11 +3,10 @@ import java.util.HashMap;
 public class RentalInfo {
 
   public String statement(Customer customer) {
-    HashMap<String, Movie> movies = new HashMap();
-    movies.put("F001", new Movie("You've Got Mail", "regular"));
-    movies.put("F002", new Movie("Matrix", "regular"));
-    movies.put("F003", new Movie("Cars", "childrens"));
-    movies.put("F004", new Movie("Fast & Furious X", "new"));
+
+    MovieLibrary movieLibrary = MovieLibrary.getInstance();
+
+    HashMap<String, Movie> movies = movieLibrary.getMovies();
 
     double totalAmount = 0;
     int frequentEnterPoints = 0;
